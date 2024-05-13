796098e33613273a4821bca35e20d99d9e9fecf5
Edenio <edenio.oliveira@grupocvlb.com.br>
2024-05-13 11:21:57 -0300

    Creation of the entities

---
+++ src/main/java/br/com/attus/sgp/entity/Pessoa.java
@@ -0,0 +1,32 @@
+package br.com.attus.sgp.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import lombok.Data;
+
+@Data
+@Entity
+public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nomeCompleto;
+    private String dataNascimento;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Endereco> enderecos = new ArrayList<>();
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Endereco enderecoPrincipal;
+
+}
