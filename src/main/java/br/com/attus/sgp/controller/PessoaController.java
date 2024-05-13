aafc838cd05b2c7e16ea9c838c73610fdb38728e
Edenio <edenio.oliveira@grupocvlb.com.br>
2024-05-13 12:10:03 -0300

    Creating Add Person Controller

--- src/main/java/br/com/attus/sgp/controller/PessoaController.java
+++ src/main/java/br/com/attus/sgp/controller/PessoaController.java
@@ -1,5 +1,29 @@
 package br.com.attus.sgp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.attus.sgp.entity.Pessoa;
+import br.com.attus.sgp.repository.PessoaRepository;
+
+@RestController
+@RequestMapping(value = "/pessoas")
 public class PessoaController {
+	
+	
+	public class UsuarioController {
+	    
+	    @Autowired
+	    private PessoaRepository repository;
+	    
+	    @GetMapping(value = "/{id}")
+	    public ResponseEntity<Pessoa> consultaPorId(@PathVariable Long id){
+	        return ResponseEntity.ok(repository.findById(id).get());
+	    }
+	}
 
 }
